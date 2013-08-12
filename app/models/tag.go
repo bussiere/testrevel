3cc20922d62806f95ee06bcad9fc5015c188d9c3
bussiere <bussiere@gmail.com>
2013-08-12 16:02:47 +0200

    models

---
+++ app/models/tag.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"github.com/robfig/revel"
+)
+
+type Hotel struct {
+	TagId          int
+	TagName   string
+}
+
+func (hotel *Hotel) Validate(v *revel.Validation) {
+	v.Check(hotel.Name,
+		revel.Required{},
+		revel.MaxSize{50},
+	)
+
+	v.MaxSize(hotel.Address, 100)
+
+	v.Check(hotel.City,
+		revel.Required{},
+		revel.MaxSize{40},
+	)
+
+	v.Check(hotel.State,
+		revel.Required{},
+		revel.MaxSize{6},
+		revel.MinSize{2},
+	)
+
+	v.Check(hotel.Zip,
+		revel.Required{},
+		revel.MaxSize{6},
+		revel.MinSize{5},
+	)
+
+	v.Check(hotel.Country,
+		revel.Required{},
+		revel.MaxSize{40},
+		revel.MinSize{2},
+	)
+}
